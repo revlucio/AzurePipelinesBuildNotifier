1e83fb46c8049aa4a97e3e78028c950e0f61b95f
Luke Elliott <revlucio@gmail.com>
2018-10-26 12:17:47 +0100

    added basic tray icon functionality

--- AzurePipelines.BuildNotifier/App.xaml.cs
+++ AzurePipelines.BuildNotifier/App.xaml.cs
@@ -1,9 +1,20 @@
-﻿namespace AzurePipelines.BuildNotifier
+﻿using System.Windows;
+using Hardcodet.Wpf.TaskbarNotification;
+
+namespace AzurePipelines.BuildNotifier
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App
     {
+        private TaskbarIcon _notifyIcon;
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            _notifyIcon = (TaskbarIcon) FindResource("NotifyIcon");
+        }
     }
 }
